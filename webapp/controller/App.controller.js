ba45e3b4c4088c115ea997de49e770030c984652
2292067 <2292067@cognizant.com>
2025-06-01 21:37:05 +0530

    notifications popover

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,30 @@
 sap.ui.define([
-	'sap/ui/core/mvc/Controller'
-], function (Controller) {
+	'sap/ui/core/mvc/Controller',
+	'sap/ui/core/Fragment',
+], function (Controller, Fragment) {
 
 	return Controller.extend('zenployee.controller.App', {
 		onInit: function () {},
+
+		onShellBarNotificationsPressed (oEvent) {
+			const oButton = oEvent.getParameter('button'),
+				oView = this.getView()
+
+			// create popover
+			if (!this._pPopover) {
+				this._pPopover = Fragment.load({
+					id: oView.getId(),
+					name: 'zenployee.fragments.Notifications',
+					controller: this
+				}).then(function (oPopover) {
+					oView.addDependent(oPopover)
+					oPopover.bindElement('/')
+					return oPopover
+				})
+			}
+			this._pPopover.then(function (oPopover) {
+				oPopover.openBy(oButton)
+			})
+		},
 	})
 })
